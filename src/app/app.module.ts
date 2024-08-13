1d7e58881c103c05e8c854ce5e636b6a4aa677e1
MoHaMMedYaSSeRRR <mohamed972001yasser@gmail.com>
2024-08-13 18:37:09 +0300

    sec commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,21 +10,30 @@ import { CarouselModule } from 'ngx-owl-carousel-o';
 import { ContactComponent } from './Componnent/contact/contact.component';
 import { FooterComponent } from './Componnent/footer/footer.component';
 import { CountryComponent } from './Componnent/country/country.component';
-
+import { LoginComponent } from './Componnent/login/login.component';
+import { RegisterComponent } from './Componnent/register/register.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ContactComponent,
     FooterComponent,
-    CountryComponent
+    CountryComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule,
     BrowserAnimationsModule,
-    CarouselModule
+    CarouselModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
