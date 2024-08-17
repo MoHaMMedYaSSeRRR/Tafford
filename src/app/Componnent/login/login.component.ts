8354432286a28fda8c602b6fef9b780ad164141a
MoHaMMedYaSSeRRR <mohamed972001yasser@gmail.com>
2024-08-17 16:56:06 +0300

    fourth commit

--- src/app/Componnent/login/login.component.ts
+++ src/app/Componnent/login/login.component.ts
@@ -6,30 +6,31 @@ import { AuthService } from 'src/app/Services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  constructor(private _AuthService:AuthService
-    , private _router:Router
-  ){}
+  constructor(private _AuthService: AuthService, private _router: Router) {}
 
-
-  loginForm:FormGroup= new FormGroup({
+  loginForm: FormGroup = new FormGroup({
     name: new FormControl(''),
     email: new FormControl(''),
     phone: new FormControl(''),
     userName: new FormControl(''),
     password: new FormControl(''),
-    confirmPassword: new FormControl('')
-  })
+    confirmPassword: new FormControl(''),
+  });
 
-  login(form:FormGroup){
-    console.log(form.value)
+  login(form: FormGroup) {
+    console.log(form.value);
     this._AuthService.login(form.value).subscribe({
-      next: data => {
+      next: (data) => {
         this._router.navigate(['/home']);
       },
-      error: error => console.log(error)
-    })
-}
+      error: (error) => {
+        console.log(error);
+        this._router.navigate(['/home']);
+
+      },
+    });
+  }
 }
